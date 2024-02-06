7953f0edce03e0061ebd9ce805f383defd04cab1
Vedanttrivedi <vedtrive@gmail.com>
2024-02-06 11:22:59 +0530

    updated code to check whether file is executable or not

--- code.sh
+++ code.sh
@@ -4,10 +4,15 @@ echo "data passed from command line is $1"
 if test -f $1
 then
 	echo "it is file"
-
+	
+elif test -x $1
+then
+	echo "file is executable"
+	
 elif test -d $1
 then
 	echo "it is directory"
+
 else
 	echo "it is neither file or directory"
 fi
